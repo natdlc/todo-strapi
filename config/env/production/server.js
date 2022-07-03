066579b0680d090ad52f78887ed220571405e05e
natdlc <nate.corpuz1@gmail.com>
2022-07-04 06:35:05 +0800

    edit

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,10 @@
-module.exports = ({ env }) => ({
+module.exports = ({ env }) => {
+    console.log(env);
+    
+    return ({
     proxy: true,
     url: env('MY_HEROKU_URL'),
     app: { 
       keys: env.array('APP_KEYS')
     },
-  })
+  })}
