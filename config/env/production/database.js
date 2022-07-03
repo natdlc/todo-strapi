7b2fe38e317c2834cf453b14ffd26a1e31a070bf
natdlc <nate.corpuz1@gmail.com>
2022-07-04 07:57:38 +0800

    edit

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,12 +2,6 @@ const parse = require("pg-connection-string").parse;
 const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => {
-
-    // console.log('env below')
-    // console.log(env)
-    // console.log('process nev below')
-    // console.log(process.env.DATABASE_URL);
-    console.log(config);
     return {
     connection: {
       client: "postgres",
